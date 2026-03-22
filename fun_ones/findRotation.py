221c02e8183e0a0418f0358e5adfae350591868c
Viijval <159607092+Viijval@users.noreply.github.com>
2026-03-22 17:21:09 +0530

    Update findRotation.py

--- fun_ones/findRotation.py
+++ fun_ones/findRotation.py
@@ -26,3 +26,10 @@ class Solution:
             if mat == target:
                 return True
         return False
+
+'''
+n//2 and n+1 //2 takes care of 'seeds'. how many seeds are there for the matrix.
+The inner loop which swaps 4 are fixed and dependent on i and j.   Because of that symmetry, one seed from the top-left quadrant is enough to "find" the other three positions via the formula.
+You never need to visit those other positions as seeds, they'd just re-do the same swap and undo it.
+n//2 for rows : you only need the top half. The bottom half is already covered as the "other side" of each cycle.
+(n+1)//2 for columns : for even n this is the same as n//2 (a square quarter). For odd n it rounds up by one to include the center column, because the center column's cycles don't have a mirror on the right hence they fold back on themselve
